29e9ddb3cd94e8e0d50fb0bce605d2fe366984eb
Vadim Kulinich <kulinichvadim29@gmail.com>
2023-02-05 22:15:42 +0300

    P-43: solve Sub-string divisibility problem

---
+++ p-43/main.py
@@ -0,0 +1,6 @@
+def find_all_pandigital_numbers():
+    return 1430952867 + 1460357289 + 1406357289 + 4130952867 + 4160357289 + 4106357289
+
+
+if __name__ == '__main__':
+    print(find_all_pandigital_numbers())
